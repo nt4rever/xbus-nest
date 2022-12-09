3b4abae2beab0a03787e462fce19211b1bbfc0e9
nt4rever <levantanald@gmail.com>
2022-12-09 15:10:44 +0700

    add some feature user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,6 @@
+import { RolesGuard } from './guard/roles.guard';
+import { Role } from './../enum/role.enum';
+import { Roles } from './decorator/roles.decorator';
 import { JwtGuard } from './guard';
 import {
   Body,
@@ -10,16 +13,16 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthDTO, SignUpDTO } from './dto';
+import { AuthDTO, EditDTO, SignUpDTO } from './dto';
 import { GetUser } from './decorator/get-user.decorator';
 import { RefreshTokenGuard } from './guard/refreshToken.guard';
 import { Request } from 'express';
+import { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(JwtGuard)
   @Post('signup')
   signup(@Body() dto: SignUpDTO) {
     return this.authService.signup(dto);
@@ -44,5 +47,21 @@ export class AuthController {
     const userId = req.user['sub'];
     const refreshToken = req.user['refreshToken'];
     return this.authService.refreshTokens(userId, refreshToken);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(JwtGuard, RolesGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('update')
+  updateAuth(@Body() dto: EditDTO, @GetUser() user: User) {
+    return this.authService.update(dto, user);
+  }
+
+  @Roles(Role.Admin)
+  @UseGuards(JwtGuard, RolesGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('users')
+  getUsers() {
+    return this.authService.getUsers();
   }
 }
